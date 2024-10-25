a8fd3008aad21edb33f79ee71c2f16f69b4948b5
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-25 18:13:52 +0300

    324. Storing the Extracted Data in Text Files

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import selectorlib as sl
 from scrape import scrape
 
@@ -10,10 +11,32 @@ def extract(source: str) -> dict:
     return value
 
 
+def store(extracted: str):
+    with open("data.txt", "a") as file:
+        file.write(f"{extracted}\n")
+
+
+def check(extracted: str):
+    if not os.path.isfile("data.txt"):
+        open("data.txt", "w").close()
+    with open("data.txt", "r") as file:
+        data = file.read()
+        if extracted in data:
+            return True
+        return False
+
+
+def send_email():
+    print("Email sent")
+
+
 print(f"Scraping the page: {URL}")
 success, scraped = scrape(URL)
 if success:
     extracted = extract(scraped)
+    if extracted and extracted != "No upcoming tours" and not check(extracted):
+        store(extracted)
+        send_email()
     print(extracted)
 else:
     print(f"Failed to scrape the page:\n\n {scraped[:100]}")
