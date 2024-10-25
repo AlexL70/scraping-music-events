94e3d80c4bdf21e1590591db80b55af975970209
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-25 17:17:15 +0300

    323. Extracting Data from the Scraped Content

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-import selectorlib
+import selectorlib as sl
 from scrape import scrape
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
 
+def extract(source: str) -> dict:
+    extractor = sl.Extractor.from_yaml_file("extract.yml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+
 print(f"Scraping the page: {URL}")
-success, source = scrape(URL)
+success, scraped = scrape(URL)
 if success:
-    print(source[:100])
+    extracted = extract(scraped)
+    print(extracted)
 else:
-    print(f"Failed to scrape the page:\n\n {source[:100]}")
+    print(f"Failed to scrape the page:\n\n {scraped[:100]}")
