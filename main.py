f8795d64591386b90914d1c08ece0823c00e8e7e
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-27 14:07:43 +0200

    331. Select, Add, and Delete SQL Data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import sqlite3
 import selectorlib as sl
 from scrape import scrape
 from send_email import send_email
@@ -13,19 +13,36 @@ def extract(source: str) -> dict:
     return value
 
 
+def get_cursor() -> sqlite3.Cursor:
+    connection = sqlite3.connect("./data.db")
+    cursor = connection.cursor()
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS events (band TEXT, city TEXT, date TEXT)")
+    return connection.cursor()
+
+
 def store(extracted: str):
-    with open("data.txt", "a") as file:
-        file.write(f"{extracted}\n")
+    band, city, date = extracted.split(", ")
+    cursor = get_cursor()
+    cursor.execute(
+        f"INSERT INTO events (band, city, date) VALUES ('{band}', '{city}', '{date}')"
+    )
+    cursor.connection.commit()
+    cursor.close()
 
 
-def check(extracted: str):
-    if not os.path.isfile("data.txt"):
-        open("data.txt", "w").close()
-    with open("data.txt", "r") as file:
-        data = file.read()
-        if extracted in data:
-            return True
-        return False
+def check(extracted: str) -> bool:
+    band, city, date = extracted.split(", ")
+    cursor = get_cursor()
+    cursor.execute(
+        f"SELECT * FROM events WHERE band = '{band}' AND city = '{city}' AND date = '{date}'"
+    )
+    if cursor.fetchone():
+        result = True
+    else:
+        result = False
+    cursor.close()
+    return result
 
 
 while True:
@@ -39,4 +56,4 @@ while True:
         print(extracted)
     else:
         print(f"Failed to scrape the page:\n\n {scraped[:100]}")
-    time.sleep(1800)
+    time.sleep(5)
